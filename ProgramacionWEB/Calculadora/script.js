a16ab66bb97ec879551832575b8941abdc85c214
MarcossDP <marcosdpaolera@gmail.com>
2022-12-23 20:18:53 -0300

    first commit

---
+++ ProgramacionWEB/Calculadora/script.js
@@ -0,0 +1,6 @@
+let precio1 = prompt("¿Cuál es el precio del primer producto?");
+let precio2 = prompt("¿Cuál es el precio del segundo producto?");
+let precio3 = prompt("¿Cuál es el precio del tercer producto?");
+let precio4 = prompt("¿Cuál es el precio del último producto?");
+var preciototal = +precio1 + +precio2 + +precio3 + +precio4;
+alert("El precio total es " + preciototal);
